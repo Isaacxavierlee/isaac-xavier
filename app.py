a18a2db6b077a7b0dda551785c1f231d972de45a
Isaacxavierlee <101383340+Isaacxavierlee@users.noreply.github.com>
2023-05-29 05:06:28 +0000

    updated

--- app.py
+++ app.py
@@ -1,10 +1,42 @@
-from flask import Flask, render_template 
+from flask import Flask, render_template , jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+{
+  'id':1,
+  'title': 'Data analyst',
+  'location': ' Orchard ,Singapore',
+  'salary': '$10,000/month'
+},
+  {
+  'id':2,
+  'title': 'Data Scientist',
+  'location': ' Paragon ,Singapore',
+  'salary': '$12,000/month'  
+  },
+{
+  'id':3,
+  'title': 'Financial analyst',
+  'location': ' Orchard ,Singapore',
+  'salary': '$10,000/month'
+},
+  {
+  'id':4,
+  'title': 'Portfolio Mananger',
+  'location': ' Orchard ,Singapore',
+  'salary': '$14,000/month'  
+  }
+]
+
+
 @app.route('/')
 def hello_world():
-    return render_template('home.html') 
+    return render_template('home.html', jobs=JOBS,company_name='Isaac') 
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
